1cf8ee3f7b3f2bca2494443dd6cbbacf8bfc9641
M. Cullen McClellan <cullenmcclellan@gmail.com>
2021-03-09 16:25:30 -0500

    Style change to top, Banner basics created, Header background removed

--- src/components/top-sub/banner.js
+++ src/components/top-sub/banner.js
@@ -1,12 +1,17 @@
 import React from 'react'
-import bannerimage from '../../img/mountcode.png'
+import bannerimage from '../../img/mountcodeReduced.png'
 
 const Banner = (props) => {
 
 
     return (
         <div className="Banner">
-            
+            <div className="CenterBannerContent">
+                <h1>Full Stack Software Engineer<br/>
+                with a Background in Buisness,<br/> 
+                Graphic Design Competence, <br/>
+                and a Passion for Continuous Learning.</h1>
+            </div>
         </div>
     )
 }
