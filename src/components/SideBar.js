e78dae15f7a3a09b58ea0d8c5062c770ef380905
Cullen McClellan <cullenmcclellan@gmail.com>
2022-02-09 02:28:40 -0500

    [MCMD-7] set min height to screen

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -1,7 +1,7 @@
 import React from "react";
 import logo from "../img/personalLogo-small-blue.svg"
 
-const topLevelDivStyling = "flex flex-col w-0 sm:w-24 max-w-max h-auto bg-one-dark-gutterGrey shadow-xl invisible sm:visible float-left"
+const topLevelDivStyling = "flex flex-col w-0 sm:w-24 max-w-max h-auto min-h-screen bg-one-dark-gutterGrey shadow-xl invisible sm:visible float-left"
 const logoDivStyling = "flex rounded-full bg-rocket-blue-700 m-2"
 const imgStyling = "w-16 min-h-20 m-2 p-2"
 
