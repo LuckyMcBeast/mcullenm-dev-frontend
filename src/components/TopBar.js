b95e24906b5d1eb7e685f41bdb3e5e100a0b1fe1
Cullen McClellan <cullenmcclellan@gmail.com>
2022-02-25 04:28:03 -0500

    [MCMD-21] Added slash at end of blogs in Vercel, redeploy

--- src/components/TopBar.js
+++ src/components/TopBar.js
@@ -1,15 +1,16 @@
 import React from "react";
 import HeaderTextAsCode from "./HeaderTextAsCode";
 
-const topLevelDivStyling = "flex flex-row-reverse sm:w-full bg-one-dark-black h-32 sm:h-20 filter drop-shadow-xl p-2"
-const headerTextDivStyling = "flex p-1 mr-1 min-h-full"
+const topLevelDivStyling =
+  "flex flex-row-reverse sm:w-full bg-one-dark-black h-32 sm:h-20 filter drop-shadow-xl p-2";
+const headerTextDivStyling = "flex p-1 mr-1 min-h-full";
 
-export default function TopBar(){
-    return(
-        <div className={topLevelDivStyling} data-testid="topBar">
-            <div className={headerTextDivStyling} data-testid="title">
-                <HeaderTextAsCode/>
-            </div>
-        </div>
-    )
+export default function TopBar() {
+  return (
+    <div className={topLevelDivStyling} data-testid="topBar">
+      <div className={headerTextDivStyling} data-testid="title">
+        <HeaderTextAsCode />
+      </div>
+    </div>
+  );
 }
