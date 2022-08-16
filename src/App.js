aaf68b392c561ee55f77cd483d0ffedbc6568bec
Cullen McClellan <cullenmcclellan@gmail.com>
2022-08-16 13:45:26 -0400

    [MCMD-29] trigger env update

--- src/App.js
+++ src/App.js
@@ -1,22 +1,21 @@
-import './style/style.css'
+import "./style/style.css";
 import SideBar from "./components/SideBar";
 import MainContent from "./components/MainContent";
 import React from "react";
 
-const appLevelStyling = "p-0 bg-mine-shaft-500 w-screen grid max-w-screen"
-const topLevelDivStyling = "flex flex-row w-auto"
+const appLevelStyling = "p-0 bg-mine-shaft-500 w-screen grid max-w-screen";
+const topLevelDivStyling = "flex flex-row w-auto";
 
-//TODO: Implement react router for blogs and about 
+//TODO: Implement react router for blogs and about
 function App() {
-
-    return (
-        <div className={appLevelStyling}>
-            <div className={topLevelDivStyling}>
-                <SideBar/>
-                <MainContent/>
-            </div>
-        </div>
-    );
+  return (
+    <div className={appLevelStyling}>
+      <div className={topLevelDivStyling}>
+        <SideBar />
+        <MainContent />
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
