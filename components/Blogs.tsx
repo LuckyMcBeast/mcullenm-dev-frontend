c1a3bf6c26294d16495d1c798a39743920e67d02
Cullen McClellan <cullen.mcclellan@mcullenm.dev>
2023-01-18 21:12:39 -0500

    [MCMD-33] order blogs by publish date desc

--- components/Blogs.tsx
+++ components/Blogs.tsx
@@ -9,13 +9,24 @@ interface Props {
     blogs: Blog[]
 }
 
+function publishDateAsDate(publishDate: string): Date {
+  const [month, day, year] = publishDate.split('-')
+  return new Date(+year, +month -1, +day)
+}
+
+function sortBlogsByDate(blogs: Blog[]): Blog[] {
+  return blogs.sort((a, b) => {
+    return publishDateAsDate(b.publishDate).getTime() - publishDateAsDate(a.publishDate).getTime()
+  })
+}
+
 export default function Blogs(props: Props) {
   if (props.blogs.length === 0 || props.blogs === undefined) {
     return <h2 className="text-one-dark-white text-center p-10 font-jetBrains">Well this is embarrassing...</h2>
   }
   return (
     <div className={topLevelDivStyling} data-testid="blogs">
-      {props.blogs.map((data: Blog) => {
+      {sortBlogsByDate(props.blogs).map((data: Blog) => {
         return (
         <Link key={data.blogId} href={`/blog/${data.blogId}`}>
             <a><BlogCard key={data.blogId.toString()} blogData={data} /></a>
